3da6689ff6fbfd22c3da825af4b829d0b785a0d4
Dejan <54706884+dejansky@users.noreply.github.com>
2022-04-04 19:10:21 +0200

    Cleaning up base files

--- platform/backend-application/src/PresentationLayer/app.js
+++ platform/backend-application/src/PresentationLayer/app.js
@@ -6,35 +6,20 @@ require('dotenv').config()
 
 module.exports = function ({ chargersRouter, transactionsRouter, reservationsRouter, authenticationRouter, adminRouter, chargePointsRouter, ocppInterface }) { //authenticationRouter
 
-    app.set('views', '/backend-app/src/presentation-layer/views')
-    app.engine('.hbs', expressHandlebars({ extname: '.hbs' }));
-    app.set('view engine', 'hbs')
-    app.engine("hbs", expressHandlebars({
-        defaultLayout: 'main.hbs'
-    }))
+
     app.use(bodyParser.urlencoded({ extended: false }))
     app.use(bodyParser.json())
-    app.use(function (request, response, next) {
-        console.log(request.method, request.url);
 
+    app.use(function (request, response, next) {
         response.setHeader("Access-Control-Allow-Origin", "*") // "localhost:3000"
         response.setHeader("Access-Control-Allow-Methods", "*") // GET, POST, PUT, DELETE
         response.setHeader("Access-Control-Allow-Headers", "*")
         response.setHeader("Access-Control-Expose-Headers", "*")
-
         next()
     })
 
 
-    app.get('/', (req, res) => {
-        res.render('index.hbs')
-    })
     app.use('/chargers', chargersRouter)
-    app.use('/transactions', transactionsRouter)
-    app.use('/reservations', reservationsRouter)
-    app.use('/chargePoints', chargePointsRouter)
-    app.use('/auth', authenticationRouter)
-    app.use('/auth/admin', adminRouter)
 
     return app
 }
