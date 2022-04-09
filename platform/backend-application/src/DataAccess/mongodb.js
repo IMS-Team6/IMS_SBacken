60781d34bcc6fa9813b580b680184399f4caa40a
Dejan <54706884+dejansky@users.noreply.github.com>
2022-04-09 21:25:10 +0200

    #B1.1.8 Added init file for mongo db, added some code structure

--- platform/backend-application/src/DataAccess/mongodb.js
+++ platform/backend-application/src/DataAccess/mongodb.js
@@ -1,27 +1,42 @@
-const { MongoClient } = require('mongodb');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+const { MongoClient } = require("mongodb");
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
 
-// Database Name
-const dbName = 'mongodb';
 
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
 
-  // the following code examples can be pasted here...
+//***************Is going to be useful when migrating to dedicated host*********** */
+// Replace the following with values for your environment.
+const username = encodeURIComponent("root"); 
+const password = encodeURIComponent("password"); 
+const clusterUrl = "localhost:27017"; 
+const authMechanism = "DEFAULT"; 
+// Replace the following with your MongoDB deployment's connection string.
+//***************Is going to be useful when migrating to dedicated host*********** */
 
-  return 'done.';
+
+
+const uri = "mongodb://root:password@mongodb:27017/admin"
+  //`mongodb+srv://${username}:${password}@${clusterUrl}/?authMechanism=${authMechanism}`;
+
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+
+//Test the connection to server, check log!
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("mongodb").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    console.log("Close connection");
+    await client.close();
+  }
 }
 
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+run();
+
+  module.exports = client;
--- platform/backend-application/src/PresentationLayer/app.js
+++ platform/backend-application/src/PresentationLayer/app.js
@@ -1,31 +1,16 @@
 const express = require("express")
 const app = express()
 
-const dbURI = "mongodb+srv://Intelligenta:Demo12@cluster0.qmb43.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-const MongoClient = require('mongodb').MongoClient
-const client = new MongoClient(dbURI,{
-    useNewUrlparser:true,
-    useUnifiedTopology:true
+
+
+
+  module.exports = function ({ restAPI }) { 
+
     
-})
-
-
-module.exports = function ({ restAPI }) { 
-
     const app = express()
 
-
-    app.use(function (request, response, next) {
-        response.setHeader("Access-Control-Allow-Origin", "*") // "localhost:3000"
-        response.setHeader("Access-Control-Allow-Methods", "*") // GET, POST, PUT, DELETE
-        response.setHeader("Access-Control-Allow-Headers", "*")
-        response.setHeader("Access-Control-Expose-Headers", "*")
-        next()
-    })
-
-
-    app.use('/Routers/rest-api', restAPI)
+    app.use('/api/', restAPI)
 
     return app
 }
