211a05d86cf906467604dd265d1212d9c83c8fdc
Dejan <54706884+dejansky@users.noreply.github.com>
2022-04-27 16:01:38 +0200

    #B1.1.3 Added queries for sessions, position and collision

--- platform/backend-application/src/DataAccess/sessionRepository.js
+++ platform/backend-application/src/DataAccess/sessionRepository.js
@@ -3,9 +3,8 @@ const { query } = require('express')
 const dbClient = require('./mongodb')
 
 module.exports = function() {
-    const exports = {}
-
-    //This is a test function!
+    const exports = {};
+    // Returns array of sessions with sessionID, robotState, collision true||false
     exports.getSessions = async function() {
         await dbClient.connect();
         const sessions = dbClient.db("mongodb").collection("session");
@@ -17,8 +16,8 @@ module.exports = function() {
 
     exports.getSessionWithID = async function(thisSessionID) {
         await dbClient.connect();
-        const sessions = dbClient.db("mongodb").collection("session")
-        const result = await sessions.findOne({ sessionID: thisSessionID })
+        const sessions = dbClient.db("mongodb").collection("session");
+        const result = await sessions.findOne({ sessionID: thisSessionID });
 
         dbClient.close();
         return result;
@@ -28,11 +27,16 @@ module.exports = function() {
     exports.createSession = async function(sessionData) {
         await dbClient.connect();
         const sessions = dbClient.db("mongodb").collection("session");
+        //This duplicates the entier object from mongoDB without _id
         var duplicate = await sessions.find({ sessionID: '' }, { _id: 0 });
 
-        //Should
-        duplicate.sessionID = sessionData.sessionID
+        // Set the values from sessinData to the duplicated object
+        duplicate.sessionID = sessionData.sessionID;
+        // duplicate.robotState = sessionData.robotState;
+        // duplicate.positions.posX.push(sessionData.positions.posX);
+        // duplicate.positions.posY.push(sessionData.positions.posY);
 
+        // Insert the duplicated object, mongoDB will generate new unique _id (Not to be confused with sessionID)
         await sessions.insert(duplicate);
 
         const result = await sessions.find({}).toArray();
@@ -43,35 +47,36 @@ module.exports = function() {
 
     exports.insertPositions = async function(sessionData) {
         //This is mock data
-        sessionData = {
+        sessionDataMock = {
             sessionID: '123',
+            robotState: 'Moving',
             positions: {
                 posX: 1,
                 posY: 2
             }
-        }
+        };
 
         await dbClient.connect();
         const sessions = dbClient.db("mongodb").collection("session");
-        await sessions.updateOne({ sessionID: sessionData.sessionID }, { $push: { "positions.posX": sessionData.positions.posX, "positions.posY": sessionData.positions.posY } });
+        const result = await sessions.updateOne({ sessionID: sessionDataMock.sessionID }, { $push: { "positions.posX": sessionDataMock.positions.posX, "positions.posY": sessionDataMock.positions.posY } });
 
-        return
+        return result
     }
 
     exports.collisionOccured = async function(collisionData) {
         //This is mock data
-        collisionData = {
+        collisionDataMock = {
             sessionID: '123',
             collisionPos: {
                 collX: 1,
                 collY: 2
             }
-        }
+        };
         await dbClient.connect();
         const sessions = dbClient.db("mongodb").collection("session");
-        await sessions.updateOne({ sessionID: collisionData.sessionID }, { $push: { "collisionPos.collX": collisionData.collisionPos.collX, "collisionPos.collY": collisionData.collisionPos.collY } });
+        const result = await sessions.updateOne({ sessionID: collisionDataMock.sessionID }, { $push: { "collisionPos.collX": collisionDataMock.collisionPos.collX, "collisionPos.collY": collisionData.collisionPos.collY } });
 
-        return
+        return result
     }
 
 
