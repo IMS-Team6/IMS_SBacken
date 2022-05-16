29651ba5d140a62c30a6f5a8b9b06268d4dd5075
Osman Said <said.osman7991@gmail.com>
2022-05-16 11:41:00 +0200

    done with robotState validations

--- platform/backend-application/src/BusinessLogic/sessionManager.js
+++ platform/backend-application/src/BusinessLogic/sessionManager.js
@@ -43,46 +43,26 @@ module.exports = function({ sessionValidation, sessionRepository }) {
 
         const exists = await sessionRepository.getSessionWithID(sessionData.sessionID)
         if (!exists) {
-
-            console.log("Session exists!!!!")
             const createSessionId = await sessionRepository.createSessionWithID(sessionData);
             callback(errors, createSessionId);
             return;
         }
+        console.log("sessionMAN: Session exists!!!!")
         const writePosition = await sessionRepository.writePositions(sessionData);
         
         if(writePosition.length > 0){
-            console.log("After writing positions..")
+            console.log("Error while writing positions..")
             writePosition.forEach(error => {
                 errors.push(error);
-                callback(errors, null);
-                return;
-        });
+            });
+            
+            callback(errors, null);
+            return;
+        };
+
+        console.log("Successfully written positions..")
         callback(errors, writePosition);
         return;
-    };
-
-    // exports.manageGetSessionRobotState = async function(sessionData, callback) {
-    //     const errors = [];
-    //     positionValidation.validateSessionID(sessionData).forEach(error => {
-    //         errors.push(error);
-    //     });
-    //     if (errors.length > 0) {
-    //         callback(errors, null);
-    //         return;
-    //     }
-    //     try {
-    //         //const getState = await sessionRepository.getSessionRobotState(sessionData);
-    //         const previousState = await sessionRepository.getSessionWithID(errors);
-    //         if (getState == previousState) {
-    //             callback(null, "Move");
-    //         } else
-    //             callback(null, "Stop");
-    //         return;
-
-    //     } catch (error) {
-    //         callback(error, null)
-    //     }
     }
     return exports;
 }
--- platform/backend-application/src/DataAccess/sessionRepository.js
+++ platform/backend-application/src/DataAccess/sessionRepository.js
@@ -13,16 +13,6 @@ module.exports = function() {
         return result;
     }
 
-    // exports.getSessionRobotState = async function(sessionID) {
-    //     //return dbClient.serverStatus()
-    //     await dbClient.connect()
-    //     const sessions = dbClient.db("mongodb").collection("session");
-    //     const result = await sessions.findOne({ robotState })
-    //     console.log(result);
-    //     dbClient.close();
-    //     return result;
-    // }
-
     exports.getSessionWithID = async function(thisSessionID) {
         await dbClient.connect();
         const sessions = dbClient.db("mongodb").collection("session");
@@ -31,6 +21,7 @@ module.exports = function() {
         dbClient.close();
         return result;
     };
+
     //Call this function on robotState: START
     exports.createSessionWithID = async function(sessionData) {
         await dbClient.connect();
@@ -61,24 +52,30 @@ module.exports = function() {
         await dbClient.connect();
         const sessions = dbClient.db("mongodb").collection("session");
         var dublicate = await sessions.findOne({ sessionID: sessionData.sessionID}, { _id: 0 });
-        if(dublicate.robotState == "STOP" || sessionData.robotState == "START"){
+        console.log("förra robotstate: " + dublicate.robotState)
+        console.log("nya robotstate: " + sessionData.robotState)
+
+        if(dublicate.robotState == "STOP" && (sessionData.robotState == "START" || sessionData.robotState == "MOVING")){
+            console.log("Error robot state.....")
             return ["wrongRobotState"];
         }
 
-        let query = {};
-
-        console.log("Trying to write positions...")
-        if (sessionData.collision === true) {
-            console.log("pushing X value ")
-            query = { $set: { collision: sessionData.collision }, $push: { "positions.posX": sessionData.positions.posX, "positions.posY": sessionData.positions.posY, "collisionPos.collX": sessionData.positions.posX, "collisionPos.collY": sessionData.positions.posY } };
-        } else {
-            query = { $push: { "positions.posX": sessionData.positions.posX, "positions.posY": sessionData.positions.posY } };
-        };
-        const result = await sessions.updateOne({ sessionID: sessionData.sessionID }, query);
-
-        console.log("writing result: " + result)
-        dbClient.close();
-        return result;
+        try{
+            let query = {};
+            console.log("Trying to write positions...")
+            if (sessionData.collision === true) {
+                console.log("pushing X value ")
+                query = { $set: { robotState: sessionData.robotState, collision: sessionData.collision }, $push: { "positions.posX": sessionData.positions.posX, "positions.posY": sessionData.positions.posY, "collisionPos.collX": sessionData.positions.posX, "collisionPos.collY": sessionData.positions.posY } };
+            } else {
+                query = { $set: { robotState: sessionData.robotState }, $push: { "positions.posX": sessionData.positions.posX, "positions.posY": sessionData.positions.posY } };
+            };
+            const result = await sessions.updateOne({ sessionID: sessionData.sessionID}, query);
+            console.log("writing result: " + result)
+            dbClient.close();
+            return result;
+        }catch{
+            return ["internalError"]
+        }
     };
 
     return exports;
